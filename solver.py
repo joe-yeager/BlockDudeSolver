c8a3a7dbec44f4d9a0c15a59852fdb1bd5ef218d
joeYeager <jayeag00@gmail.com>
2015-10-31 20:02:45 -0700

    It is now able to solve puzzles that require blocks, but no block reuse

--- solver.py
+++ solver.py
@@ -165,7 +165,7 @@ class Solver:
         self.nw = [[0,0,1,3],[0,0,2,3]]
         self.ne = [[0,0,4,1],[0,0,4,2]]
         self.pickUp = [[0,0,4,2],[0,0,2,3]]
-        self.drop = [[2,0,4,0], [0,2,0,3]]
+        self.drop = [[0,0,4,0], [0,0,0,3]]
         self.sw = [[0,3,0,1],[0,3,0,2]]
         self.se = [[4,0,1,0],[4,0,2,0]]
         self.fall = [[3,1,0,1],[3,1,0,0],[3,0,0,0],[3,2,0,1],[3,2,0,0],[3,1,0,2],[3,2,0,2],[3,0,0,1],[3,0,0,2],
@@ -185,10 +185,26 @@ class Solver:
         self.obstacleFlag = False
         self.obstacleHeight = 0
         self.blocksRequired = 0
+        self.availableBlocks = 0
+        self.blockGoals = []
+        self.blockLocs = []
         self.trapFlag = False
         self.moveQuadrants = []
         self.moveList = []
     
+    def prettyPrintLevel(self):
+        lower = 0
+        upper = 0
+        temp = []
+        length = len(self.level.layout)
+        for i in range(0, length/self.level.width):
+            upper += self.level.width
+            for k in range(lower, upper):
+                temp.append(self.level.layout[k])
+            print(temp)
+            temp = []
+            lower += self.level.width
+
     def setLevel(self,level):
         self.level = Level(level.width,level.height,list(level.layout) )
         self.currentLevel = Level(self.level.width, self.level.height, list(self.level.layout) )
@@ -285,15 +301,40 @@ class Solver:
         for i in range (1, self.obstacleHeight):
             tempI = self.obstableIndex - (i * self.modifier)
             for j in range (i, self.obstacleHeight):
-                tempI -= width
+                tempI += self.level.width
                 if self.level.layout[tempI] == EMPY:
+                    self.blockGoals.append(tempI)
                     self.blocksRequired += 1
+    
+    def findBlocks(self, level):
+        tempI = self.obstableIndex - 1
+        tempX = tempI % level.width
+        self.availableBlocks = 0
+        for i in range (1, level.height+1):
+            endRow = i * level.width
+            for j in range (tempX, endRow):
+                if level.layout[j] == BLCK:
+                    self.blockLocs.append(j)
+                    self.availableBlocks += 1
+            tempX += level.width
 
-    def checkObstacles(self):
+    def findClosestBlock(self):
+        self.closest = sys.maxint
+        for i in range(0,len(self.blockLocs)):
+            dist = self.blockLocs[i] - self.player.index
+            # dist %= self.level.width
+            if abs(dist) < self.closest:
+                self.closest = dist
+
+    def checkObstacles(self, level):
         self.taxiCabDistance()
         self.checkObstaclesHelper(0,0,0,self.player.index + self.modifier)
         if self.obstacleFlag:
             self.calculateBlocksRequired()
+            self.findBlocks(level)
+            if self.availableBlocks < self.blocksRequired:
+                print "Level is unsolvable, not enough blocks"
+                self.victory = True
 
     def generateMoveQuads(self):
         i,l,w = self.player.index, self.level.layout, self.level.width
@@ -305,33 +346,80 @@ class Solver:
         self.moveQuadrants.append([ pg[3], pg[4], pg[6], pg[7] ])
         self.moveQuadrants.append([ pg[4], pg[5], pg[7], pg[8] ])
 
-
-
     def analyzeMoveQuads(self, move):
-
-        if self.level.layout[self.player.index] == WEST and self.modifier == -1:
-            if move in self.fall:
-                self.quadMoves.append("fall")
-            elif move in self.west:
-                self.quadMoves.append("w")
-            elif move in self.nw:
-                self.quadMoves.append("nw")
-            elif move in self.sw:
-                self.quadMoves.append("sw")
-
-        elif self.level.layout[self.player.index] == EAST and self.modifier == 1:
-            if move in self.fall:
-                self.quadMoves.append("fall")
-            elif move in self.east:
-                self.quadMoves.append("e")
-            elif move in self.ne:
-                self.quadMoves.append("ne")
-            elif move in self.se:
-                self.quadMoves.append("se")
+        if move in self.fall:
+            self.quadMoves.append("fall")
+        if move in self.west:
+            self.quadMoves.append("w")
+        if move in self.nw:
+            self.quadMoves.append("nw")
+        if move in self.sw:
+            self.quadMoves.append("sw")
+        if move in self.east:
+            self.quadMoves.append("e")
+        if move in self.ne:
+            self.quadMoves.append("ne")
+        if move in self.se:
+            self.quadMoves.append("se")
+        if move in self.faceWest:
+            self.quadMoves.append("fw")
+        if move in self.faceEast:
+            self.quadMoves.append("fe")
+        if move in self.pickUp and not self.player.isHoldingBlock:
+            self.quadMoves.append("pickup")
+        if move in self.drop and self.player.isHoldingBlock:
+            self.quadMoves.append("drop")
 
     def selectMove(self, moveCode):
         self.performMove(self.level, self.moveFuncs[moveCode][0], self.moveFuncs[moveCode][1])
         self.moveList.append(moveCode)
+
+    def prioritizeWest(self):
+        if "w" in self.quadMoves:
+            self.selectMove("w")
+        elif "nw" in self.quadMoves:
+            self.selectMove("nw")
+        elif "sw" in self.quadMoves:
+            self.selectMove("sw")
+
+    def prioritizeEast(self):
+        if "e" in self.quadMoves:
+            self.selectMove("e")
+        elif "ne" in self.quadMoves:
+            self.selectMove("ne")
+        elif "se" in self.quadMoves:
+            self.selectMove("se")
+
+    def solveObstacle(self):
+        self.findClosestBlock()
+        playerWest = self.player.index - 1
+        playerEast = self.player.index + 1
+        if self.player.isHoldingBlock:
+            if self.player.dir == WEST and playerWest in self.blockGoals:
+                self.level.layout[playerWest] = BLCK
+                self.selectMove("drop")
+                self.blockGoals.remove(playerWest)
+                self.obstacleFlag = False
+                self.checkObstacles(self.level)
+            if self.player.dir == EAST and playerEast in self.blockGoals:
+                self.level.layout[playerEast] = BLCK
+                self.selectMove("drop")
+                self.blockGoals.remove(playerEast)
+                self.obstacleFlag = False
+                self.checkObstacles(self.level)
+        if "pickup" in self.quadMoves and not self.player.isHoldingBlock and self.obstacleFlag:
+            if self.player.dir == WEST:
+                self.level.layout[playerWest] = EMPY
+                removeValue = playerWest
+            elif self.player.dir == EAST:
+                self.level.layout[playerEast] = EMPY
+                removeValue = playerEast
+            self.selectMove("pickup")
+            # self.blockLocs.remove(removeValue)
+        elif self.closest < 0:  #block is to the west
+            self.prioritizeWest()
+        elif self.closest > 0:  #block is to the east
+            self.prioritizeEast()
 
     def pickMove(self):
         if "fall" in self.quadMoves: # if fall is a valid move, it must be chosen.
@@ -341,24 +429,16 @@ class Solver:
         elif not self.obstacleFlag: # no obstables, pick moves that will get you closer to goal
             self.player.falling = False
             if self.modifier == -1:  # goal is west
-                if "w" in self.quadMoves:
-                    self.selectMove("w")
-                elif "nw" in self.quadMoves:
-                    self.selectMove("nw")
-                elif "sw" in self.quadMoves:
-                    self.selectMove("sw")
-
-
+                self.prioritizeWest()
             else:
-                if "e" in self.quadMoves:
-                    self.selectMove("e")
-                elif "ne" in self.quadMoves:
-                    self.selectMove("ne")
-                elif "se" in self.quadMoves:
-                    self.selectMove("se")
+                self.prioritizeEast()
         
-        else:  #obstacles, fun starts
+        else:
             self.player.falling = False
+            self.solveObstacle()
+
+
+
         self.quadMoves = []
     def solve(self):
         self.quadMoves = []
@@ -367,7 +447,7 @@ class Solver:
             print("Level is unsolvable, player or door do not exist")
             return
 
-        self.checkObstacles()
+        self.checkObstacles(self.level)
 
         while not self.victory:
             self.generateMoveQuads()
@@ -375,6 +455,7 @@ class Solver:
                 self.checkVictory(move)
                 self.analyzeMoveQuads(move)
             self.pickMove()
+
         print("Solved!!!")
 
     def resetState(self):
@@ -383,6 +464,12 @@ class Solver:
     def stepThroughSolution(self):
         print(self.moveList)
         currentMove = self.moveList.pop(0)
+        if currentMove == "drop":
+            if self.player.dir == WEST:
+                self.currentLevel.layout[self.player.index - 1] = BLCK
+            else:
+                self.currentLevel.layout[self.player.index + 1] = BLCK
+
         self.performMove(self.currentLevel, self.moveFuncs[currentMove][0], self.moveFuncs[currentMove][1])
 
 
